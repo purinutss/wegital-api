85c0c2015a4c3c9337060eb529d9b2227bdf47ba
purinutss <purinuts2541@gmail.com>
2023-04-24 14:42:01 +0700

    get all proportion and by user id

--- src/controllers/proportion-controller.js
+++ src/controllers/proportion-controller.js
@@ -15,3 +15,25 @@ exports.createProportion = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getProportionByUserId = async (req, res, next) => {
+  try {
+    const userProportion = await Proportion.findOne({
+      where: {
+        id: req.user.id
+      }
+    });
+    res.status(200).json({ userProportion });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getAllProportion = async (req, res, next) => {
+  try {
+    const proportions = await Proportion.findAll();
+    res.status(200).json({ proportions });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routes/proportion-route.js
+++ src/routes/proportion-route.js
@@ -5,5 +5,6 @@ const proportionController = require("../controllers/proportion-controller");
 const router = express.Router();
 
 router.post("/", proportionController.createProportion);
-
+router.get("/", proportionController.getAllProportion);
+router.get("/user", proportionController.getProportionByUserId);
 module.exports = router;
