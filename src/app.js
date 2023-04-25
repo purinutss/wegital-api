526cb4a4a669abda64947bff66a81a04095a900c
purinutss <purinuts2541@gmail.com>
2023-04-25 17:26:41 +0700

    reate limit and helmet and error middleware

--- src/app.js
+++ src/app.js
@@ -5,8 +5,11 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const authenticateMiddleware = require("./middlewares/authenticate");
+const errorMiddleware = require("./middlewares/error");
 
 const authRoute = require("./routes/auth-route");
 const proportionRoute = require("./routes/proportion-route");
@@ -16,9 +19,20 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 1000,
+    message: { message: "many requested, please try again" }
+  })
+);
+
 app.use("/auth", authRoute);
 app.use("/proportion", authenticateMiddleware, proportionRoute);
 app.use("/user", authenticateMiddleware, userRoute);
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 6789;
 app.listen(port, () => console.log(`server run on port ${port}`));
